6db108a55e89eff0f646a845c885b4fb78d561b2
John Wiseman <jjwiseman@gmail.com>
2016-01-31 17:36:35 -0800

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
 
 setup(
     name='lemapp',
-    version='0.2',
+    version='0.3.dev0',
     description='lemonodor Google-alike app framework.',
     long_description=(read('README.rst') +
                       read('HISTORY.rst') +
