65442b35cb0179a305495c84b809268f55c0c3bc
John Wiseman <jjwiseman@gmail.com>
2017-06-06 21:50:51 -0700

    Preparing release 0.5

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,7 +2,7 @@ Changelog
 =========
     
 
-0.5 (unreleased)
+0.5 (2017-06-06)
 ----------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*rnames):
 
 setup(
     name='lemapp',
-    version='0.5.dev0',
+    version='0.5',
     description='lemonodor Google-alike app framework.',
     long_description=(read('README.rst') +
                       read('HISTORY.rst') +
